4f470342590df571dc691860c6fb61809efa5529
chy <781288772@qq.com>
2022-04-27 23:10:03 +0800

    循环渲染

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,10 +1,38 @@
+import "./App.css";
 
-import './App.css';
+//1.识别常规变量
+//2.原生方法调用
+//3.三元运算符
+//4.react 完成列表渲染 map
+const name = "小黑";
+const getAge = () => {
+  return 18;
+};
+const flag = true;
+const persons = [
+  {
+    id: 1,
+    name: "张三",
+  },
+  {
+    id: 2,
+    name: "李四",
+  },
+  {
+    id: 3,
+    name: "王五",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-     app
+      {" "}
+      {name} {getAge()} {flag ? "真棒" : "不行"}{" "}
+
+      <ul> 
+      {persons.map(person=>  <li key={person.id}>{person.name}</li>)}
+      </ul>
     </div>
   );
 }
