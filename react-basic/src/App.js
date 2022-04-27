9b98ed537feadff16d10c9e5ac32b2af0e909477
chy <781288772@qq.com>
2022-04-27 23:20:23 +0800

    条件渲染

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,38 +1,21 @@
 import "./App.css";
+//条件渲染
+//技术方案：三元表达式（常用）逻辑&&运算
 
-//1.识别常规变量
-//2.原生方法调用
-//3.三元运算符
-//4.react 完成列表渲染 map
-const name = "小黑";
-const getAge = () => {
-  return 18;
-};
+//1.三元表达式 - 满足条件才渲染一个span标签
 const flag = true;
-const persons = [
-  {
-    id: 1,
-    name: "张三",
-  },
-  {
-    id: 2,
-    name: "李四",
-  },
-  {
-    id: 3,
-    name: "王五",
-  },
-];
 
 function App() {
   return (
     <div className="App">
-      {" "}
-      {name} {getAge()} {flag ? "真棒" : "不行"}{" "}
+      {flag ? (
+        <div>
+          <span>满足条件</span>
+        </div>
+      ) : null}
 
-      <ul> 
-      {persons.map(person=>  <li key={person.id}>{person.name}</li>)}
-      </ul>
+
+      {true && <span>this is span</span>}
     </div>
   );
 }
